184c502dc83145532df7f3602c9143f0962b70c2
kevinguo09 <jackie.guo1991@gmail.com>
2016-03-30 12:30:22 -0400

    Change

---
+++ src/main/java/test1/test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test1;
+
+/**
+ * @author yujiaguo
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("gitHub");
+	}
+
+}
